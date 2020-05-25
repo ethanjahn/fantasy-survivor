fe81e9a1e89279303618f437e23542ec66fa5346
Ethan Jahn <ethanjahn@gmail.com>
2020-05-24 19:24:55 -0500

    Updated folder structure

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,54 @@
 module.exports = function(api) {
   api.cache(true);
+
+  const presets = ['babel-preset-expo'];
+  const plugins = [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ["./src/"],
+        alias: {
+          "_assets": "./src/assets",
+          "_components": "./src/components",
+          "_atoms": "./src/components/atoms",
+          "_molecules": "./src/components/molecules",
+          "_organisms": "./src/components/organisms",
+          "_navigations": "./src/navigations",
+          "_scenes": "./src/scenes",
+          "_services": "./src/services",
+          "_styles": "./src/styles",
+          "_utils": "./src/utils"
+        }
+      }
+      
+    ]
+  ];
+
   return {
-    presets: ['babel-preset-expo'],
+    presets,
+    plugins
   };
 };
+
+const plugins = [
+  [
+    require.resolve('babel-plugin-module-resolver'),
+    {
+      root: ["./src/"],
+      alias: {
+        "_assets": "./src/assets",
+        "_components": "./src/components",
+        "_atoms": "./src/components/atoms",
+        "_molecules": "./src/components/molecules",
+        "_organisms": "./src/components/organisms",
+        "_navigations": "./src/navigations",
+        "_scenes": "./src/scenes",
+        "_services": "./src/services",
+        "_styles": "./src/styles",
+        "_utils": "./src/utils"
+      }
+    }
+    
+  ]
+
+];
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {HelloWorld} from '_atoms';
+
+const App = () => <HelloWorld name="Helder Burato Berto" />;
+
+export default App;

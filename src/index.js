f2953ed8c08f22aeeb0e1430752defef8a8255d7
Ethan Jahn <ethanjahn@gmail.com>
2020-05-24 20:46:08 -0500

    Added basic navigation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import {HelloWorld} from '_atoms';
 
-const App = () => <HelloWorld name="Helder Burato Berto" />;
+function HomeScreen() {
+    return (
+        <HelloWorld name="Helder Burato Berto" />
+    );
+  }
 
-export default App;
+const Stack = createStackNavigator();
+
+export default function App() {
+    return (
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      );
+}

4608df9fc4ee0382ff73ecc0ce11a5af1387d2bb
mxgagneja <mxgagneja@gmail.com>
2020-05-26 22:09:09 -0500

    Beginning of Team Standings in Leaderboard

--- src/screens/leaderboard.js
+++ src/screens/leaderboard.js
@@ -1,8 +1,35 @@
 import * as React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, View, Text } from "react-native";
 
-export default function LeaderboardScreen() {
-  return <SafeAreaView>
-    <Text style={{fontWeight: 'bold', color: '#51355A',fontSize: 40, margin: 20, textAlign: 'center'}}>Previously on...</Text>
-    </SafeAreaView>;
-}
+const TeamStandingBar = props =>
+  <Text style={{fontSize: 35}}>
+    <Text>{props.Standing}</Text>
+    <Text>{props.TeamName}</Text>
+    <Text>{props.Wins}-{props.Ties}-{props.Losses}</Text>
+  </Text>;
+
+class TeamStanding extends React.Component {
+  state = {
+      Standing: 1,
+      TeamName: 'Tony\'s Ladder',
+      Wins: 2,
+      Ties: 1,
+      Losses: 1
+  }
+
+  render() {
+    return <TeamStandingBar 
+      Standing={this.state.Standing}
+      TeamName={this.state.TeamName}
+      Wins={this.state.Wins}
+      Ties={this.state.Ties}
+      Losses={this.state.Losses} />
+    }
+  }
+  
+  export default function LeaderboardScreen() {
+    return <SafeAreaView>
+      <Text style={{fontWeight: 'bold', color: '#51355A',fontSize: 40, margin: 20, textAlign: 'center'}}>Previously on...</Text>
+      <TeamStanding />
+      </SafeAreaView>
+  }

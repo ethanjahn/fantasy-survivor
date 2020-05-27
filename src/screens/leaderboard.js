23455fca9d41cf68d6568a03fbceeb5260576112
mxgagneja <mxgagneja@gmail.com>
2020-05-26 21:06:22 -0500

    Added heading to Leaderboard screen. #quickwins

--- src/screens/leaderboard.js
+++ src/screens/leaderboard.js
@@ -1,6 +1,8 @@
 import * as React from "react";
-import { Text } from "react-native";
+import { SafeAreaView, Text } from "react-native";
 
 export default function LeaderboardScreen() {
-  return <Text>Leaderboard</Text>;
+  return <SafeAreaView>
+    <Text style={{fontWeight: 'bold', color: '#51355A',fontSize: 40, margin: 20, textAlign: 'center'}}>Previously on...</Text>
+    </SafeAreaView>;
 }

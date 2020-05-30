d9d7624c179af9073f98e6b905653a70365beb81
mxgagneja <mxgagneja@gmail.com>
2020-05-30 18:53:12 -0500

    First team standing bar added to Leadership screen

--- src/screens/leaderboard.js
+++ src/screens/leaderboard.js
@@ -1,12 +1,18 @@
 import * as React from "react";
 import { SafeAreaView, View, Text } from "react-native";
+import { HelloWorld } from '_atoms';
 
 const TeamStandingBar = props =>
-  <Text style={{fontSize: 35}}>
-    <Text>{props.Standing}</Text>
-    <Text>{props.TeamName}</Text>
-    <Text>{props.Wins}-{props.Ties}-{props.Losses}</Text>
-  </Text>;
+  <View style={{flexDirection: 'row'}}>
+    <View style={{flex: 8, flexDirection: 'row', justifyContents: 'space-around'}}>
+        <Text style={{flex: 3, fontSize: 35, textAlign: 'center', textAlignVertical: 'center'}}>{props.Standing}</Text>
+        <Text style={{flex: 7, fontSize: 20, textAlignVertical: 'center'}}>{props.TeamName}</Text>
+        <Text style={{flex: 5, fontSize: 18, textAlign: 'center', textAlignVertical: 'center'}}>{props.Wins}-{props.Ties}-{props.Losses}</Text>
+    </View>
+    <View style={{flex: 1, flexDirection: 'row'}}>
+        <HelloWorld />
+        </View>
+  </View>
 
 class TeamStanding extends React.Component {
   state = {
@@ -14,22 +20,26 @@ class TeamStanding extends React.Component {
       TeamName: 'Tony\'s Ladder',
       Wins: 2,
       Ties: 1,
-      Losses: 1
+      Losses: 1,
+      name: 'Mehek'
   }
 
   render() {
-    return <TeamStandingBar 
+    return  <View>
+      <TeamStandingBar 
       Standing={this.state.Standing}
       TeamName={this.state.TeamName}
       Wins={this.state.Wins}
       Ties={this.state.Ties}
-      Losses={this.state.Losses} />
+      Losses={this.state.Losses}
+      name={this.state.name} />
+      </View>
     }
   }
   
   export default function LeaderboardScreen() {
     return <SafeAreaView>
-      <Text style={{fontWeight: 'bold', color: '#51355A',fontSize: 40, margin: 20, textAlign: 'center'}}>Previously on...</Text>
+      <Text style={{fontWeight: 'bold', color: '#51355A',fontSize: 30, margin: 20, textAlign: 'center'}}>Previously on...</Text>
       <TeamStanding />
       </SafeAreaView>
   }
